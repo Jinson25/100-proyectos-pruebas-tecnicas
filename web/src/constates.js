af01a25adaa19f0b932d4121932b05dfe701159c
Jinson25 <am987406@gmail.com>
2024-12-24 13:29:16 -0500

    Refactor estructura de componentes y eliminar archivos innecesarios

---
+++ web/src/constates.js
@@ -0,0 +1,22 @@
+export const TAGS = [
+    {
+        title: "Github",
+        link: "https://github.com",
+        class: "bg-blue-500 text-white",
+    },
+    {
+        title: "Preview",
+        link: "https://preview.com",
+        class: "bg-blue-500 text-white",
+    },
+];
+export const PROJECTS = [
+    {
+        title: "Juego de Pong en 2D",
+        description:
+            "Este proyecto incluye dos paletas controladas por el jugador, una bola que rebota en las paredes y las paletas, y un sistema de puntuación. Lógica de colisiones en JavaScript.",
+        tangs: TAGS,
+        image:
+            "https://codewithfaraz.com/img/how%20to%20create%20a%20ping%20pong%20game%20with%20html,%20css%20and%20javascript.jpg",
+    },
+];
